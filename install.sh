5b8c1fa9feac1cbdfcd5e79c46200f076566e15f
TranceMelan <95452879+TranceMelan@users.noreply.github.com>
2024-07-09 17:56:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 # Install browser chrome
 npx puppeteer browsers install chrome
